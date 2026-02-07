fe4ac77ef2e0e061c27eb6be98369d61c8cff2da
shreya-pandey03 <reachatpandey@gmail.com>
2026-02-07 19:46:42 +0530

    initial commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,15 +1,10 @@
-// import axios from "axios";
-
-// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-
-// export const axiosInstance = axios.create({
-//   baseURL: BASE_URL,
-//   withCredentials: true, // send cookies with the request
-// });
-
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
+  baseURL: BASE_URL,
+  withCredentials: true, // send cookies with the request
 });
+
+
