f40345ff763ea41ff5265c4913b5047f75760b22
shreya-pandey03 <reachatpandey@gmail.com>
2026-02-12 00:17:11 +0530

    NexCall

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -55,12 +55,7 @@ const Sidebar = () => {
         <div className="flex items-center gap-3">
           <div className="avatar">
             <div className="w-10 rounded-full">
-              {/* <img src={authUser?.profilePic} alt="User Avatar" /> */}
-              <img
-                src={`https://api.dicebear.com/7.x/avataaars/svg?seed=${authUser._id}`}
-                alt="User Avatar"
-                referrerPolicy="no-referrer"
-              />
+              <img src={authUser?.profilePic} alt="User Avatar" />
             </div>
           </div>
           <div className="flex-1">
